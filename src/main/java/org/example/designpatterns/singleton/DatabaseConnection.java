ae7a7acf58013d307eb32e8f531dc986a908e5cb
Dheeraj <potdardheerajs@gamil.com>
2026-08-20 14:14:48 +0530

    Implemented Singleton design pattern

---
+++ src/main/java/org/example/designpatterns/singleton/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package org.example.designpatterns.singleton;
+
+public class DatabaseConnection {
+    private static DatabaseConnection instance;
+    private DatabaseConnection(){
+
+    }
+    public static DatabaseConnection getInstance(){
+        if(instance==null){
+            instance=new DatabaseConnection();
+        }
+        return instance;
+    }
+}
---
+++ src/main/java/org/example/designpatterns/singleton/Main.java
@@ -0,0 +1,11 @@
+package org.example.designpatterns.singleton;
+
+
+public class Main {
+    public static void main(String []args){
+        DatabaseConnection d1= DatabaseConnection.getInstance();
+        DatabaseConnection d2 = DatabaseConnection.getInstance();
+
+        System.out.println(d1==d2);
+    }
+}
